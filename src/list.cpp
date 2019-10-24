a6cdccdc8aac4cc689e4d792b377f0a74189d667
Anna Bernas <bernas.anna@icloud.com>
2019-10-24 18:12:48 +0200

    Rename the doublyLinkedList to list

--- src/list.cpp
+++ src/list.cpp
@@ -7,43 +7,43 @@
 
 int main()
 {
-    /********  MY DoublyLinkedList **********/
+    /********  MY list **********/
     try{
-        DoublyLinkedList<int> DoublyLinkedList;
+        list<int> list;
 
         std::cout<<"CHECKS IF INSERT METHODS, CONST ITERATOR WORKS \n \n";
-        DoublyLinkedList.push_back(4);
-        DoublyLinkedList.push_back(3);
-        DoublyLinkedList.push_front(1);
-        DoublyLinkedList.push_front(2);
-        DoublyLinkedList.insert(9,2);
+        list.push_back(4);
+        list.push_back(3);
+        list.push_front(1);
+        list.push_front(2);
+        list.insert(9,2);
         std::cout<<"output should be: 2 1 9 4 3 \n";
         std::cout<<"real output: ";
 
-        for(auto it = DoublyLinkedList.cbegin(); it!=DoublyLinkedList.cend(); ++it)
+        for(auto it = list.cbegin(); it!=list.cend(); ++it)
             std::cout<<*it<<"  ";
 
 
         std::cout<<"\n\nCHECKS IF [] OPERATOR WORKS \n";
-        std::cout<<"DoublyLinkedList[3] = 0;"<<std::endl;
-        DoublyLinkedList[3] = 0;
-        for(auto it = DoublyLinkedList.cbegin(); it!=DoublyLinkedList.cend(); ++it)
+        std::cout<<"list[3] = 0;"<<std::endl;
+        list[3] = 0;
+        for(auto it = list.cbegin(); it!=list.cend(); ++it)
             std::cout<<*it<<"  ";
 
-        std::cout<<"\nDoublyLinkedList[1] = "<<DoublyLinkedList[1]<<std::endl;
+        std::cout<<"\nlist[1] = "<<list[1]<<std::endl;
 
         std::cout<<"\n\nCHECKS IF REMOVE METHOD WORKS \n";
-        DoublyLinkedList.remove(2);
-        for(auto it = DoublyLinkedList.cbegin(); it!=DoublyLinkedList.cend(); ++it)
+        list.remove(2);
+        for(auto it = list.cbegin(); it!=list.cend(); ++it)
             std::cout<<*it<<"  ";
 
 
         std::cout<<"\n\nCHECKS IF ITERATOR WORKS (TO EACH ELEMENT OF COLLECTION SHOULD BE ADDED 2) \n";
-        for(auto it =DoublyLinkedList.begin() ; it != DoublyLinkedList.end(); ++it)
+        for(auto it =list.begin() ; it != list.end(); ++it)
             *it+=2;
 
 
-        for(auto it = DoublyLinkedList.cbegin(); it!=DoublyLinkedList.cend(); ++it)
+        for(auto it = list.cbegin(); it!=list.cend(); ++it)
             std::cout<<*it<<"  ";
 
     }
@@ -54,31 +54,31 @@ int main()
 
     /****** STL *****/
 
-    std::list<int> DoublyLinkedList;
-    DoublyLinkedList.push_back(4);
-    DoublyLinkedList.push_back(3);
-    DoublyLinkedList.push_front(1);
-    DoublyLinkedList.push_front(2);
-    auto it = DoublyLinkedList.begin();
+    std::list<int> list;
+    list.push_back(4);
+    list.push_back(3);
+    list.push_front(1);
+    list.push_front(2);
+    auto it = list.begin();
     std::advance(it,2);
-    DoublyLinkedList.insert(it,9);
+    list.insert(it,9);
 
     std::cout<<"\n\nCHECKS HOW INSERT METHODS IN STL WORK \n \n";
     std::cout<<"output should be: 2 1 9 4 3 \n";
     std::cout<<"real output: ";
 
-    std::copy(DoublyLinkedList.begin(), DoublyLinkedList.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::copy(list.begin(), list.end(), std::ostream_iterator<int>(std::cout, " "));
 
     std::cout<<"\n\nCHECKS HOW ITERATOR WORKS (TO EACH ELEMENT OF COLLECTION SHOULD BE ADDED 2) \n";
 
-    for(auto it =DoublyLinkedList.begin() ; it != DoublyLinkedList.end(); ++it)
+    for(auto it =list.begin() ; it != list.end(); ++it)
         *it+=2;
 
-    std::copy(DoublyLinkedList.begin(), DoublyLinkedList.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::copy(list.begin(), list.end(), std::ostream_iterator<int>(std::cout, " "));
 
     std::cout<<"\n\nCHECKS HOE REMOVE METHOD WORKS \n";
-    DoublyLinkedList.remove(4);
-    std::copy(DoublyLinkedList.begin(), DoublyLinkedList.end(), std::ostream_iterator<int>(std::cout, " "));
+    list.remove(4);
+    std::copy(list.begin(), list.end(), std::ostream_iterator<int>(std::cout, " "));
 
 
     return 0;
